dcf8e9bcdab4c0d67d9f640e7c9ab2676fbf3b99
sleeb29 <sfleeb@gmail.com>
2018-03-13 22:29:17 -0400

    commit unversioned files

---
+++ src/main/java/com/news/article/user/service/userservice/model/UserClientService.java
@@ -0,0 +1,43 @@
+package com.news.article.user.service.userservice.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "user_client_service")
+public class UserClientService {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "user_client_service_id")
+    long userClientServiceId;
+
+    @Column(name = "user_id")
+    long userId;
+
+    @Column(name = "client_service_id")
+    long clientServiceId;
+
+    public long getUserClientServiceId() {
+        return userClientServiceId;
+    }
+
+    public void setUserClientServiceId(long userClientServiceId) {
+        this.userClientServiceId = userClientServiceId;
+    }
+
+    public long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(long userId) {
+        this.userId = userId;
+    }
+
+    public long getClientServiceId() {
+        return clientServiceId;
+    }
+
+    public void setClientServiceId(long clientServiceId) {
+        this.clientServiceId = clientServiceId;
+    }
+}
---
+++ src/main/java/com/news/article/user/service/userservice/service/UserService.java
@@ -0,0 +1,22 @@
+package com.news.article.user.service.userservice.service;
+
+import com.news.article.user.service.userservice.model.ClientService;
+import com.news.article.user.service.userservice.model.Topic;
+import com.news.article.user.service.userservice.model.User;
+
+import java.util.Set;
+
+public interface UserService {
+
+    void createUser(User user);
+
+    void updateUser(User user);
+
+    void deleteUser(User user);
+
+    Set<Topic> findAllTopics();
+    Set<ClientService> findAllClientServices();
+
+    public boolean isUser(User user);
+
+}
